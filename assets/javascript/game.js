0f83f27471adc7a6d72b972ac775b8e007c01dce
Phil Mauracher <gradientus@gmail.com>
2019-07-02 16:27:23 -0600

    Everything working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,59 +26,38 @@ var letterArray = [
   "y",
   "z"
 ];
+var computerGuess = letterArray[Math.floor(Math.random() * 26)]; //computer guess
 var winText = document.getElementById("win");
 var lossText = document.getElementById("loss");
 var guessesLeftText = document.getElementById("guessesLeft");
 var userGuessesText = document.getElementById("userGuesses");
-var computerGuess = letterArray[Math.floor(Math.random() * 26)];
 var wins = 0;
 var losses = 0;
+var maxGuesses = 9;
 
 //this grabs the users key input
 document.onkeyup = function(event) {
-  var userGuess = event.key.toLowerCase();
-  var numberOfGuesses = 9;
+  var userGuess = event.key.toLowerCase(); //set keypress to userGuess
+  console.log("computer:" + computerGuess);
+  console.log("user:" + userGuess);
 
-  //this tells the game what to do if the user guesses correctly
-  function winningFunction() {
+  if (userGuess === computerGuess && maxGuesses > 1) {
     wins++;
-    numberOfGuesses = 9;
+    maxGuesses = 9;
     winText.textContent = "Wins: " + wins;
-    guessesLeftText.textContent = "Guesses Left: " + numberOfGuesses;
-  }
-
-  //this give 9 chances for the user to guess the right letter
-  function guessAgainFunction() {
-    numberOfGuesses--;
-    guessesLeftText.textContent = "Guesses Left: " + numberOfGuesses;
-    userGuessesText.append(userGuess);
-  }
-
-  //this tells the game what do do when guesses have expired and still no correct guess
-  function loseFunction() {
+    guessesLeftText.textContent = "Guesses Left: " + maxGuesses;
+    userGuessesText.textContent = "Your Guesses So Far:"; //shouldn't this clear the append?
+  } else if (userGuess !== computerGuess && maxGuesses === 1) {
     losses++;
-    numberOfGuesses = 9;
-    winText.textContent = "Losses: " + losses;
-    guessesLeftText.textContent = "Guesses Left: " + numberOfGuesses;
-    userGuessesText.textContent = "Your Guesses So Far: ";
-  }
-
-  //this will tell us which function to run based on the right guess or not
-  if (numberOfGuesses > 0) {
-    if (userGuess === computerGuess) {
-      winningFunction();
-    } else {
-      guessAgainFunction();
-    }
+    maxGuesses = 9;
+    lossText.textContent = "Losses: " + losses;
+    guessesLeftText.textContent = "Guesses Left: " + maxGuesses;
+    userGuessesText.textContent = "Your Guesses So Far:"; //shouldn't this clear the append?
   } else {
-    loseFunction();
+    maxGuesses--;
+    guessesLeftText.textContent = "Guesses Left: " + maxGuesses;
+    userGuessesText.append(userGuess);
   }
 };
 
-//this is where I'm stuck.
-//Wins works perfect.
-//Appending guesses works kind of.
-//Decreasing guesses only works to 8.
-//Losses doesn't work.
-
-//I'm guessing that I need to run a loop but I'm unclear where to put it.
+//everything working but the guesses so far.  How do I clear that after a win or loss?
