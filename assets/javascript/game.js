8ea705c5b54f0d22ad18f60cef57a6bee55fbcb7
Phil Mauracher <gradientus@gmail.com>
2019-07-02 10:04:34 -0600

    Commit what I have.  Having trouble with the game logic and how to get past 8 guesses and endless appends.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,9 +26,59 @@ var letterArray = [
   "y",
   "z"
 ];
+var winText = document.getElementById("win");
+var lossText = document.getElementById("loss");
+var guessesLeftText = document.getElementById("guessesLeft");
+var userGuessesText = document.getElementById("userGuesses");
 var computerGuess = letterArray[Math.floor(Math.random() * 26)];
+var wins = 0;
+var losses = 0;
 
+//this grabs the users key input
 document.onkeyup = function(event) {
   var userGuess = event.key.toLowerCase();
-  var numberOfGuesses = 0;
+  var numberOfGuesses = 9;
+
+  //this tells the game what to do if the user guesses correctly
+  function winningFunction() {
+    wins++;
+    numberOfGuesses = 9;
+    winText.textContent = "Wins: " + wins;
+    guessesLeftText.textContent = "Guesses Left: " + numberOfGuesses;
+  }
+
+  //this give 9 chances for the user to guess the right letter
+  function guessAgainFunction() {
+    numberOfGuesses--;
+    guessesLeftText.textContent = "Guesses Left: " + numberOfGuesses;
+    userGuessesText.append(userGuess);
+  }
+
+  //this tells the game what do do when guesses have expired and still no correct guess
+  function loseFunction() {
+    losses++;
+    numberOfGuesses = 9;
+    winText.textContent = "Losses: " + losses;
+    guessesLeftText.textContent = "Guesses Left: " + numberOfGuesses;
+    userGuessesText.textContent = "Your Guesses So Far: ";
+  }
+
+  //this will tell us which function to run based on the right guess or not
+  if (numberOfGuesses > 0) {
+    if (userGuess === computerGuess) {
+      winningFunction();
+    } else {
+      guessAgainFunction();
+    }
+  } else {
+    loseFunction();
+  }
 };
+
+//this is where I'm stuck.
+//Wins works perfect.
+//Appending guesses works kind of.
+//Decreasing guesses only works to 8.
+//Losses doesn't work.
+
+//I'm guessing that I need to run a loop but I'm unclear where to put it.
