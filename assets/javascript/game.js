cd22a8b07c0dd8f46651aae7918d9977307c8db6
Phil Mauracher <gradientus@gmail.com>
2019-07-29 16:12:09 -0600

    Fixed a bug that counted down turns and increased losses if a user picked the same letter over and over

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,36 +41,29 @@ document.onkeyup = function(event) {
   var userGuess = event.key.toLowerCase(); //set keypress to userGuess
 
   //what to do when the user wins: increment wins, reset maxguesses to 9, clear previous guesses
-  if (userGuess === computerGuess && maxGuesses > 0) {
+  if (userGuess === computerGuess) {
     wins++;
     maxGuesses = 9;
+    computerGuess = letterArray[Math.floor(Math.random() * 26)];
     winText.textContent = "Wins: " + wins;
-    guessesLeftText.textContent = "Guesses Left: " + maxGuesses;
-    userGuessesText.textContent = "Your Guesses So Far:";
+    guessesLeftText.textContent = "Guesses Left: 9";
+    userGuessesText.textContent = "Your Guesses So Far: ";
     guessesArray = [];
-
-    //what do do when the user loses:  increment losses after 9 attempts, resent maxguesses to 9, clear previous guesses
   } else if (userGuess !== computerGuess && maxGuesses === 0) {
     losses++;
     maxGuesses = 9;
+    computerGuess = letterArray[Math.floor(Math.random() * 26)];
     lossText.textContent = "Losses: " + losses;
-    guessesLeftText.textContent = "Guesses Left: " + maxGuesses;
+    guessesLeftText.textContent = "Guesses Left: 9";
     userGuessesText.textContent = "Your Guesses So Far:";
     guessesArray = [];
-
-    //what to do when the user guesses incorrectly but hasn't reached 9 attempts, tell them what they guessed so far
+  } else if (guessesArray.indexOf(userGuess) >= 0) {
+    console.log(guessesArray);
   } else {
-    //the user will chose something that needs to be pushed to the array.
-    //if the user already guessed that letter do nothing, alert that the user already guessed that.
-    //if the user hasn't guessed that letter then the below and add that letter to the array
-
-    guessesArray.push = userGuess;
-
+    guessesArray.push(userGuess);
     maxGuesses--;
     guessesLeftText.textContent = "Guesses Left: " + maxGuesses;
     userGuessesText.append(userGuess);
+    console.log(guessesArray);
   }
 };
-
-// TODO Add logic so that if the user keeps guessing the same letter over and over again, it isn't recorded over and over again and doesn't count against attempts
-// TODO Might need to make an array to keep track of the user guesses to take care of this
